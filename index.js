1211914bf65cad640257430000a2113650534120
Zetorminator <gaborjozsefzeteny@gmail.com>
2026-07-23 15:28:48 +0200

    fix: update api url and clean code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true
-})
+});
 
 app.command("/goy-ping", async ({ command, ack, respond }) => {
   const start = Date.now();
@@ -21,7 +21,7 @@ app.command('/goy-meaningoflife', async ({ command, ack, respond }) => {
   const responses = [
     "The meaning of life to me is to find a purpose in this never ending misery.",
     "The meaning of life is 42!"
-  ]
+  ];
 
   const randomIndex = Math.floor(Math.random() * responses.length);
   const chosenResponse = responses[randomIndex];
@@ -34,17 +34,18 @@ app.command('/goy-meaningoflife', async ({ command, ack, respond }) => {
   } catch (error) {
     console.error('something went horrible worng, like there was and explosion wrong:', error);
   }
-})
+});
 
 app.command('/goy-fact', async ({ command, ack, respond }) => {
   await ack();
 
   try {
+  
     const apiResponse = await fetch("https://jsph.pl", {
       headers: {
         "Accept": "application/json"
       }
-    })
+    });
     
     if (!apiResponse.ok) {
       throw new Error(`API responded with status code ${apiResponse.status}`);
@@ -56,12 +57,12 @@ app.command('/goy-fact', async ({ command, ack, respond }) => {
     await respond({ 
       text: `*Did you know?* \n${randomFact}`,
       response_type: "in_channel" 
-    })
+    });
   } catch (error) {
     console.error('Error fetching random fact from API:', error.message || error);
     await respond({ text: "Sorry, the facts service is currently unavailable or returning an error page. Please try again later!" });
   }
-})
+});
 
 app.command('/goy-coin', async ({ command, ack, respond }) => {
   await ack();
@@ -78,17 +79,17 @@ app.command('/goy-coin', async ({ command, ack, respond }) => {
   } catch (error) {
     console.error('Error running coinflip command:', error);
   }
-})
+});
 
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log("bot is not just running but its sprinting to deliver some anwser to your questions!");
-})()
+})();
 
 process.on('uncaughtException', (err) => {
   console.error('The bot crashed due to an unhandled error:', err);
-})
+});
 
 process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-})
+});
